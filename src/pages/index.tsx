03faf310b52c2c10f1bcaeb2cadfcddbebba5626
Tiago Oliveira <tiagolivesan@outlook.com>
2021-02-24 12:12:53 -0300

    Aula 1 / 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
@@ -11,6 +12,7 @@ export default function Home() {
     <section>
       <div>
         <Profile />
+        <CompletedChallenges />
       </div>
       <div>
 
