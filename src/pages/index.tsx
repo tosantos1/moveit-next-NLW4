f29413d4c090031c95edf81b2dee868540279c71
Tiago Oliveira <tiagolivesan@outlook.com>
2021-02-24 22:54:01 -0300

    Aula 1 / 2 / 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
 import Head from 'next/head'
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+
+import head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
   <div className={styles.container}>
+    <head>
+      <title>GoMover</title>
+    </head>
     <ExperienceBar />
     <section>
       <div>
         <Profile />
         <CompletedChallenges />
+        <Countdown />
       </div>
       <div>
 
